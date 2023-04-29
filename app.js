89396c9afa1f461731136c59437d97e84150b5e6
NeilMisra99 <116110498+NeilMisra99@users.noreply.github.com>
2023-04-28 21:47:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ passport.use(
       {
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/home",
+        callbackURL: "http://wicked-poncho-fox.cyclic.app/auth/google/home",
         userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
       },
       function (accessToken, refreshToken, profile, cb) {
